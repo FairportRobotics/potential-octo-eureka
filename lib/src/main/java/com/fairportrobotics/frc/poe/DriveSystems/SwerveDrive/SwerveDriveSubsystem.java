ae935c5727bdcc7d701c958283da317e2db7c358
Tyler Wilcox <tyler@tywilly.com>
2022-04-08 17:56:37 -0400

    Setup library boilerplate

---
+++ lib/src/main/java/com/fairportrobotics/frc/poe/DriveSystems/SwerveDrive/SwerveDriveSubsystem.java
@@ -0,0 +1,11 @@
+package com.fairportrobotics.frc.poe.DriveSystems.SwerveDrive;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SwerveDriveSubsystem extends SubsystemBase{
+    
+    public SwerveDriveSubsystem(){
+        
+    }
+
+}
