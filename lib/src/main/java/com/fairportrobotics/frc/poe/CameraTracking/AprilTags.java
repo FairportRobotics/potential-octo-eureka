98ced09d82e94dc33fd459c39724006f076360d3
Declipsonator <96392121+Declipsonator@users.noreply.github.com>
2023-01-30 20:52:23 -0500

    resolved issues

--- lib/src/main/java/com/fairportrobotics/frc/poe/CameraTracking/AprilTags.java
+++ lib/src/main/java/com/fairportrobotics/frc/poe/CameraTracking/AprilTags.java
@@ -25,11 +25,12 @@ public class AprilTags {
         for(PhotonCamera camera : cameras) {
             for(PhotonTrackedTarget target : camera.getLatestResult().getTargets()) {
                 boolean toAdd = true;
+                List<PhotonTrackedTarget> tempTargets = targets;
                 // check if the target is already in the list and if it has a higher pose ambiguity
                 for(PhotonTrackedTarget arTarget : targets) {
                     if(target.getFiducialId() == arTarget.getFiducialId() && target.getPoseAmbiguity() < arTarget.getPoseAmbiguity()) {
                         // if it does, remove it from the list
-                        targets.remove(arTarget);
+                        tempTargets.remove(arTarget);
                         break;
                     } else if(target.getFiducialId() == arTarget.getFiducialId()) {
                         // if the target is already in the list and has the same ID, don't add it
@@ -37,10 +38,10 @@ public class AprilTags {
                         break;
                     }
                 }
+                if(toAdd) { tempTargets.add(target); }
 
-                if(toAdd) targets.add(target);
+                targets = tempTargets;
             }
-            targets.addAll(camera.getLatestResult().getTargets());
         }
 
         return targets;
@@ -48,36 +49,31 @@ public class AprilTags {
 
     // method to check if any camera has any targets
     public boolean hasTargets() {
-        for(PhotonCamera camera : cameras) {
-            // if a camera has targets, return true
-            if(camera.getLatestResult().hasTargets()) return true;
-        } 
-        
-        return false;
+        return cameras.stream().anyMatch((camera) -> camera.getLatestResult().hasTargets() == true);
     }      
 
     // method to get the closest target
     public PhotonTrackedTarget getClosestTarget() {
-        if(!hasTargets()) return null;
+        if(!hasTargets()) { return null; }
         PhotonTrackedTarget closestTarget = null;
         for(PhotonTrackedTarget target : getTargets()) {
-            if(closestTarget == null || calculateDistance(target.getBestCameraToTarget()) > calculateDistance(target.getBestCameraToTarget()) ) closestTarget = target;
+            if(closestTarget == null || calculateDistance(closestTarget.getBestCameraToTarget()) > calculateDistance(target.getBestCameraToTarget())) { closestTarget = target; }
         }
         return closestTarget;
     }
 
     // method to get the target with the lowest pose ambiguity
     public PhotonTrackedTarget getLowestAmbiguityTarget() {
-        if(!hasTargets()) return null;
+        if(!hasTargets()) { return null; }
         PhotonTrackedTarget lowestAmbiguity = null;
         for(PhotonTrackedTarget target : getTargets()) {
             // check if the target has a lower pose ambiguity than the current lowest ambiguity target
-            if(lowestAmbiguity == null || lowestAmbiguity.getPoseAmbiguity() > target.getPoseAmbiguity()) lowestAmbiguity = target;
+            if(lowestAmbiguity == null || lowestAmbiguity.getPoseAmbiguity() > target.getPoseAmbiguity()) { lowestAmbiguity = target; }
         }
         return lowestAmbiguity;
     }
 
     private double calculateDistance(Transform3d t) {
-        return Math.sqrt(t.getX()*t.getX() + t.getY()*t.getY() + t.getZ()*t.getZ());
+        return Math.sqrt(t.getX()*t.getX() + t.getY()*t.getY());
     }
 }
